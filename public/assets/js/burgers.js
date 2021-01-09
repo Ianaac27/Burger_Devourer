08e43544f231ab0476218b9df568a29f5c986f40
Ian Fleshman-Cooper <ianaac4@yahoo.com>
2021-01-09 13:41:55 -0800

    developed js function to create new burger in burger.js within the assets folder

--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -1,5 +1,5 @@
-$(function() {
-    $(".devour_button").on("click", (e) => {
+// $(function() {
+    $(".devour-button").on("click", (e) => {
         let id = $(this).data("id");
         let newDevour = $(this).data("newdevour");
 
@@ -16,6 +16,27 @@ $(function() {
         //reloads page and updates list
         location.reload();
         }
-    )
-    })
-})
+    );
+    });
+
+    $(".new-burger").on("submit", (e) => {
+        e.preventDefault();
+
+        var newBurger = {
+          burger_name: $("#burger").val().trim(),
+        };
+    
+        // Send the POST request
+        $.ajax("/api/burgers", {
+          type: "POST",
+          data: newBurger
+        }).then(
+          function() {
+            console.log("created new burger");
+            //reloads page and updates list
+            location.reload();
+          }
+        );
+    });
+
+// });
